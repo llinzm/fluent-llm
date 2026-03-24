b0b96f903c74bea318c0c20b03e502dae3d1ab2b
QuestionMarque <farzin.dadashi@gmail.com>
2026-03-24 10:03:42 +0100

    Delete tests directory

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Tests package for fluent_llm.
-
-This package contains unit tests for the components of the
-``fluent_llm`` module.  Use ``python -m unittest discover`` from the
-project root to run all tests.
-"""
