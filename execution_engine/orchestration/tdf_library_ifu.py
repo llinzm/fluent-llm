ac2a5d395c712a15aabd0da780a3a1ec2723f95e
llinzm <190534123+llinzm@users.noreply.github.com>
2026-04-10 10:13:46 +0200

    Add IFU description for library examples.

---
+++ execution_engine/orchestration/tdf_library_ifu.py
@@ -0,0 +1,32 @@
+IFU_just_tips_short = "Pick up Tips."
+IFU_just_tips_medium = "Pick up 200 ul tips on all channels."
+
+IFU_transfer_samples_plate_to_plate_short = "Transfer 50 µL from the first column of the sample plate to the same column in the dilution plate."
+IFU_transfer_samples_plate_to_plate_medium = "Pick up tips and transfer 50 µL from the first column of the sample plate to the same column in the dilution plate. Drop the tips."
+
+IFU_transfer_samples_tubes_to_plate_short = "Transfer 100 µL of sample from the tubes in the tube runner to the first column of the dilution plate."
+IFU_transfer_samples_tubes_to_plate_medium = "Get tips and transfer 100 µL of sample from the tubes in the tube runner to the first column of the dilution plate. Drop the tips."
+
+IFU_dilute_samples_from_tube_short = "Add 50 µL of diluent to the first two columns of the dilution plate. Then transfer 50 µL from each tube in the tube runner into the corresponding wells of these columns to achieve a 1:2 dilution."
+IFU_dilute_samples_from_tube_medium = "Get tips and add 50 µL of diluent to the first two columns of the dilution plate. With new tips transfer 50 µL from each tube in the tube runner into the corresponding wells of these columns to achieve a 1:2 dilution."
+
+IFU_serial_dilution_short = "Perform a serial 1:2 dilution across the dilution plate by transferring 25 µL from one column to the next, mixing after each step, and continuing this process across the entire row."
+IFU_serial_dilution_medium = "Get tips and perform a serial 1:2 dilution across the dilution plate by transferring 25 µL from one column to the next, mixing after each step, and continuing this process across the entire row. Use the same tips and empty them into the well before aspirating again. Remove the 25 ul from the last column to keep similar volumes."
+
+IFU_dilute_samples_from_full_plate_short = "Perform a 1:2 dilution by adding 25 µL of diluent to all wells of the dilution plate, followed by transferring 25 µL of sample from the sample plate into the corresponding wells of the dilution plate."
+IFU_dilute_samples_from_full_plate_medium = "Perform a 1:2 dilution by distributing 25 µL of diluent to all wells of the dilution plate, followed by transferring 25 µL of sample from the sample plate into the corresponding wells of the dilution plate."
+
+IFU_sample_tube_to_plate_replicates_short = "Transfer 50 µL from each tube in the tube rack into the plate in triplicate."
+IFU_sample_tube_to_plate_replicates_medium = "Transfer 50 µL from each tube in the tube rack into the dilution plate in triplicate, distributing the samples column-wise and arranging replicates across rows."
+
+IFU_fill_plate_hotel_short = "Distribute 50 ul of reagent A to the 96 well plate into 16 wells. Add 50 ul of sample from the sample tubes to the corresponding wells. Transfer the plate back to the hotel."
+IFU_fill_plate_hotel_medium = "Transfer a 96 well plate from the hotel to a pipetting nest. Distribute 50 ul of reagent A to the 96 well plate into 16 wells. Add 50 ul of sample from the sample tubes to the corresponding wells. Transfer the plate back to the hotel."
+
+IFU_distribute_antisera_short = "Transfer 50 ul of Antisera A to the first 4 wells of a 96 well plate and Antisera B to the next 4 wells."
+IFU_distribute_antisera_medium = "Pick up tips and aspirate 50 ul of Antisera A in the first 4 tips and Antisera B with the second 4 tips. Dispense it into the first column of a 96 well plate. Drop the tips."
+
+IFU_distribute_antigens_short = "Dispense 25 ul of Antigen A to every well of a 96 well plate."
+IFU_distribute_antigens_medium = "Get tips and aspirate 300 ul of Antigen A and dispense 25 ul to each well of a 96 well plate."
+
+IFU_add_tRBC_short = "Resuspend tRBC stock and distribute 50 ul per well into each well of a 96 well plate."
+IFU_add_tRBC_medium = "Get Tips and resuspend the tRBC stock by mixing it. Aspirate 600 ul of tRBC and dispense 50 ul per well into each well of a 96 well plate. Drop the tips."
